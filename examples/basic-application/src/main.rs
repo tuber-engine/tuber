d0a775c83bed59106824b6231477849048a40d73
Clément Sibille <claymeuns@protonmail.com>
2021-02-01 20:49:32 +0100

    Add basic-application example

---
+++ examples/basic-application/src/main.rs
@@ -0,0 +1,6 @@
+use tuber::Engine;
+
+fn main() -> tuber::Result<()> {
+    let mut engine = Engine::new();
+    engine.ignite()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,38 @@
-mod tests {
-    #[test]
-    fn it_works() {
-        assert!(true);
+use winit::{event::{Event, WindowEvent}, event_loop::{EventLoop, ControlFlow}, window::{WindowBuilder, Window}};
+
+pub struct Engine;
+
+impl Engine {
+    pub fn new() -> Engine {
+        Self
     }
+
+    pub fn ignite(&mut self) -> Result<()> {
+        let event_loop = EventLoop::new();
+        let window = WindowBuilder::new().build(&event_loop)?;
+        
+        event_loop.run(move |event, _, control_flow| {
+            *control_flow = ControlFlow::Poll;
+            match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => {
+                    *control_flow = ControlFlow::Exit
+                },
+                _ => ()
+            }
+        });
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    WinitOsError(winit::error::OsError)
+}
+
+impl From<winit::error::OsError> for Error {
+fn from(error: winit::error::OsError) -> Self { Error::WinitOsError(error) }
 }
