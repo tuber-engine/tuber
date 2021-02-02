24d1fc5eefd0a27c1e4fcbaf261e98a0a1ae4ef1
Clément Sibille <claymeuns@protonmail.com>
2021-02-02 06:39:36 +0100

    Remove warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-use winit::{event::{Event, WindowEvent}, event_loop::{EventLoop, ControlFlow}, window::{WindowBuilder, Window}};
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
 pub struct Engine;
 
@@ -9,18 +13,16 @@ impl Engine {
 
     pub fn ignite(&mut self) -> Result<()> {
         let event_loop = EventLoop::new();
-        let window = WindowBuilder::new().build(&event_loop)?;
-        
+        let _window = WindowBuilder::new().build(&event_loop)?;
+
         event_loop.run(move |event, _, control_flow| {
             *control_flow = ControlFlow::Poll;
             match event {
                 Event::WindowEvent {
                     event: WindowEvent::CloseRequested,
                     ..
-                } => {
-                    *control_flow = ControlFlow::Exit
-                },
-                _ => ()
+                } => *control_flow = ControlFlow::Exit,
+                _ => (),
             }
         });
     }
@@ -30,9 +32,11 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
-    WinitOsError(winit::error::OsError)
+    WinitOsError(winit::error::OsError),
 }
 
 impl From<winit::error::OsError> for Error {
-fn from(error: winit::error::OsError) -> Self { Error::WinitOsError(error) }
+    fn from(error: winit::error::OsError) -> Self {
+        Error::WinitOsError(error)
+    }
 }
