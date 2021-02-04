9eee9eb5215678c68a030d48263ef7f6f4633033
Clément Sibille <claymeuns@protonmail.com>
2021-02-04 08:13:58 +0100

    Add SystemBundle and GameState

--- examples/basic-application/src/main.rs
+++ examples/basic-application/src/main.rs
@@ -1,6 +1,20 @@
 use tuber::Engine;
+use tuber::GameStateBuilder;
+use tuber::SystemBundleBuilder;
+
+fn print_hello_world() {
+    println!("Hello world");
+}
 
 fn main() -> tuber::Result<()> {
     let mut engine = Engine::new();
+    let system_bundle = SystemBundleBuilder::new()
+        .with_system(Box::new(|| print_hello_world()))
+        .with_system(Box::new(|| println!("Test")))
+        .build();
+    let initial_game_state = GameStateBuilder::new()
+        .with_system_bundle(system_bundle)
+        .build();
+    engine.push_state(initial_game_state);
     engine.ignite()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,18 @@ use winit::{
     window::WindowBuilder,
 };
 
-pub struct Engine;
+pub struct Engine {
+    state_stack: Vec<GameState>,
+}
 
 impl Engine {
     pub fn new() -> Engine {
-        Self
+        Self {
+            state_stack: vec![],
+        }
     }
 
-    pub fn ignite(&mut self) -> Result<()> {
+    pub fn ignite(mut self) -> Result<()> {
         let event_loop = EventLoop::new();
         let _window = WindowBuilder::new().build(&event_loop)?;
 
@@ -22,9 +26,85 @@ impl Engine {
                     event: WindowEvent::CloseRequested,
                     ..
                 } => *control_flow = ControlFlow::Exit,
+                Event::MainEventsCleared => {
+                    self.current_state().step();
+                }
                 _ => (),
             }
         });
+    }
+
+    pub fn push_state(&mut self, state: GameState) {
+        self.state_stack.push(state);
+    }
+
+    fn current_state(&mut self) -> &mut GameState {
+        self.state_stack.last_mut().expect("No state")
+    }
+}
+
+pub struct GameState {
+    system_bundles: Vec<SystemBundle>,
+}
+
+impl GameState {
+    pub fn step(&mut self) {
+        let bundles = &self.system_bundles;
+        for system_bundle in bundles.iter() {
+            for system in system_bundle.system_functions.iter() {
+                (system)()
+            }
+        }
+    }
+}
+
+pub struct GameStateBuilder {
+    system_bundles: Vec<SystemBundle>,
+}
+
+impl GameStateBuilder {
+    pub fn new() -> Self {
+        GameStateBuilder {
+            system_bundles: vec![],
+        }
+    }
+
+    pub fn with_system_bundle(mut self, system_bundle: SystemBundle) -> Self {
+        self.system_bundles.push(system_bundle);
+        self
+    }
+
+    pub fn build(self) -> GameState {
+        GameState {
+            system_bundles: self.system_bundles,
+        }
+    }
+}
+
+pub struct SystemBundle {
+    system_functions: Vec<SystemFunction>,
+}
+
+type SystemFunction = Box<dyn Fn()>;
+
+pub struct SystemBundleBuilder {
+    systems: Vec<SystemFunction>,
+}
+
+impl SystemBundleBuilder {
+    pub fn new() -> Self {
+        SystemBundleBuilder { systems: vec![] }
+    }
+
+    pub fn with_system(mut self, system: Box<dyn Fn()>) -> Self {
+        self.systems.push(system.into());
+        self
+    }
+
+    pub fn build(self) -> SystemBundle {
+        SystemBundle {
+            system_functions: self.systems,
+        }
     }
 }
 
