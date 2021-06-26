6417ee1af822447e0df290fe5b374ce80f90576d
Clément Sibille <claymeuns@protonmail.com>
2021-06-26 14:33:00 +0200

    tuber: Expose tuber_common as common and expose tuber_physics as physics

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-pub use tuber_common::tilemap;
+pub use tuber_common as common;
 pub use tuber_core::{ecs, input::*, Engine, Error, Result, TuberRunner};
 pub use tuber_graphics as graphics;
 pub use tuber_graphics_wgpu as graphics_wgpu;
+pub use tuber_physics as physics;
 pub use tuber_winit::WinitTuberRunner;
