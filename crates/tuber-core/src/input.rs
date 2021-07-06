150b45c081d5f39deaac83b73ccfcbf86acd360f
Clément Sibille <claymeuns@protonmail.com>
2021-07-06 23:29:00 +0200

    input: Add the InputState::was() method to know if an input was active at the previous frame

--- crates/tuber-core/src/input.rs
+++ crates/tuber-core/src/input.rs
@@ -66,7 +66,9 @@ pub enum Input {
 
 pub struct InputState {
     key_state: [bool; 43],
+    previous_key_state: [bool; 43],
     mouse_button_state: [bool; 3],
+    previous_mouse_button_state: [bool; 3],
     last_mouse_position: (f32, f32),
     mouse_moved: bool,
 }
@@ -74,7 +76,9 @@ impl InputState {
     pub fn new() -> Self {
         Self {
             key_state: [false; 43],
+            previous_key_state: [false; 43],
             mouse_button_state: [false; 3],
+            previous_mouse_button_state: [false; 3],
             last_mouse_position: (0.0, 0.0),
             mouse_moved: false,
         }
@@ -90,8 +94,20 @@ impl InputState {
         }
     }
 
+    pub fn was(&self, input: Input) -> bool {
+        match input {
+            Input::KeyDown(key) => self.previous_key_state[key as usize],
+            Input::KeyUp(key) => !self.previous_key_state[key as usize],
+            Input::MouseButtonDown(button) => self.previous_mouse_button_state[button as usize],
+            Input::MouseButtonUp(button) => !self.previous_mouse_button_state[button as usize],
+            Input::MouseMotion(..) => unimplemented!(),
+        }
+    }
+
     pub fn handle_input(&mut self, input: Input) {
         self.mouse_moved = false;
+        self.previous_key_state = self.key_state.clone();
+        self.previous_mouse_button_state = self.mouse_button_state.clone();
         match input {
             Input::KeyDown(key) => self.key_state[key as usize] = true,
             Input::KeyUp(key) => self.key_state[key as usize] = false,
